e9da0ecbe4adc2792a5b481668323fca6d71beb6
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-11 23:48:15 +0530

    [Kumar Shubham]Added:Use Random and if else Function to check employee is Present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,13 @@
 //Welcome To Employee Wage Problem
+//UC1-Employee is Absent or Present
+const IS_ABSENT = 0;
+//Generate Random Number 0 or 1 
+let empCheck = Math.floor(Math.random()*2);
+if (empCheck == IS_ABSENT) 
+{
+    console.log("Employee is Absent");
+} 
+else 
+{
+    console.log("Employee is Present");
+}
